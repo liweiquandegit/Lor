8a4c83b2e5dc9b124728fe05ffbf6c7fa2df00b5
Administrator <Administrator@ZZZXIT71336>
2018-04-28 18:01:18 +0800

    1

--- Lor/Program.cs
+++ Lor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using SqlMaker;
+using Model;
+using System.Collections.Generic;
 
 namespace Lor
 {
@@ -7,22 +9,29 @@ namespace Lor
     {
         static void Main(string[] args)
         {
-            string tran = SqlProvider.CreateTransaction();
             string message = "";
-            //SqlProvider.GetDbInstance<UserModel>().Insert(new UserModel() { Id = "201201001", Code = "75125", Creator = "Liwq", Flag = 1, Name = "Liweiquan" }, tran, out message);
-            //SqlProvider.CloseTransaction(tran, true);
-            //tran = SqlProvider.CreateTransaction();
-            //UserModel usrCp = new UserModel() { Id = "201201001", Code = "75125", Creator = "Liwq", Flag = 1, Name = "Liweiquande" };
-            //IList<UserModel> usrDbs = SqlProvider.GetDbInstance<UserModel>().Select(tran, new List<OrderBy>() { new OrderBy() { Field = "Id" } }, new Limit(), out message, Restrain.Eq("Id", "201201001"));
-            //UserModel usrDb = null;
-            //if (usrDbs.Count > 0)
-            //    usrDb = usrDbs[0];
-            //usrDb.Merge(usrCp);
-            //SqlProvider.GetDbInstance<UserModel>().Update(usrCp, tran, out message);
-            //SqlProvider.CloseTransaction(tran, true);
+            //写入数据对象并提交
+            string tran = SqlProvider.CreateTransaction();
+            SqlProvider.GetDbInstance<UserModel>().Insert(new UserModel() { Id = "201201002", Code = "75125", Creator = "Liwq", Flag = 1, Name = "Liweiquan" }, tran, out message);
+            SqlProvider.CloseTransaction(tran, true);
 
-            //IList<UserWithPwdModel> models = SqlProvider.GetDbInstance<UserWithPwdModel>().Select(tran, new List<OrderBy>() { new OrderBy() { Field = "Id" } }, new Limit(), out message, Restrain.Lk("ID", "%2%"));
-            //SqlProvider.CloseTransaction(tran,true); 
+            //在新的事务中，更改数据，将数据合并到持久化对象中，最后保存提交事务
+
+            tran = SqlProvider.CreateTransaction();
+            UserModel usrCp = new UserModel() { Id = "201201001", Code = "75125", Creator = "Liwq", Flag = 1, Name = "Liweiquande" };
+            IList<UserModel> usrDbs = SqlProvider.GetDbInstance<UserModel>().Select(tran, new List<OrderBy>() { new OrderBy() { Field = "Id" } }, new Limit(), out message, Restrain.Eq("Id", "201201001"));
+            UserModel usrDb = null;
+            if (usrDbs.Count > 0)
+                usrDb = usrDbs[0];
+            usrDb.Merge(usrCp);
+            SqlProvider.GetDbInstance<UserModel>().Update(usrCp, tran, out message);
+            SqlProvider.CloseTransaction(tran, true);
+
+            //在新事务中查询自定义的模板
+            tran = SqlProvider.CreateTransaction();
+            IList<UserWithPwdModel> models = SqlProvider.GetDbInstance<UserWithPwdModel>().Select(tran, new List<OrderBy>() { new OrderBy() { Field = "Id" } }, new Limit(), out message, Restrain.Lk("ID", "%2%"));
+            SqlProvider.CloseTransaction(tran,true); 
+
             Console.Read();
         }
     }
